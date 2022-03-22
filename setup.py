f3570ec0fe03d3aba436a944b2a0a90d9fc2b776
xuquanfeng <73832500+xuquanfeng@users.noreply.github.com>
2022-03-22 18:05:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r',encoding='utf-8') as fp:
 setup(
     name='qrpca',
     version='1.4',
-    description='A Python package for PCA algorithm of QR accelerated SVD decomposition with CUDA of torch.',
+    description='A Python package for QR based PCA decomposition with CUDA acceleration via torch.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/xuquanfeng/qrpca',
